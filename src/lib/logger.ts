2c197fc4ecb9c32b21a8b90c58dc9471ce5cdba7
Dylan Smith <dylantsmith@gmail.com>
2024-11-19 15:26:55 -0600

    feat: add logger (#4)
    - add a logger library, which provides the ability to include the `functionName` in the log messages
    - add a nanoid as a `requestId` in the next.js middleware, and use this unique ID in the log messages. This allows us to link messages within the same request together.

---
+++ src/lib/logger.ts
@@ -0,0 +1,21 @@
+import config from '@/config/index';
+import { headers } from 'next/headers';
+import pino, { LoggerOptions } from 'pino';
+
+// https://github.com/pinojs/pino/blob/master/docs/api.md#pinooptions-destination--logger
+const options: LoggerOptions = {
+  base: undefined,
+  // "silent" to disable logging
+  level: process.env.NODE_ENV === 'test' ? 'silent' : config.log.level,
+  nestedKey: 'payload',
+  timestamp: pino.stdTimeFunctions.isoTime,
+};
+
+const logger = pino(options);
+
+export function getLogger(functionName?: string) {
+  const headersList = headers();
+  const requestId = headersList.get(config.headers.requestId);
+
+  return logger.child({ functionName, requestId });
+}
---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import projectConfig from '@/config/index';
+import { nanoid } from 'nanoid';
+import { NextResponse } from 'next/server';
+
+export function middleware() {
+  return NextResponse.next({
+    headers: {
+      [projectConfig.headers.requestId]: nanoid(),
+    },
+  });
+}
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
