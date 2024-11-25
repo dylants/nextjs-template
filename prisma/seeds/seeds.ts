beffa15455fd66fac7659210bf30a972be6b7c00
Dylan Smith <dylantsmith@gmail.com>
2024-11-25 13:03:08 -0600

    chore: widget seeds
    - add widgets to the seed data for the test user

--- prisma/seeds/seeds.ts
+++ prisma/seeds/seeds.ts
@@ -1,5 +1,7 @@
 import { generateUsers } from './users/users.seeds';
+import { generateWidgets } from './widgets/widgets.seeds';
 
 export default async function generateSeeds() {
   await generateUsers();
+  await generateWidgets();
 }
